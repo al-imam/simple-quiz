05ef1508d924fe3563acb4515814e9b52b2619af
al-imam <alimam01828@gmail.com>
2022-12-24 03:08:15 +0600

    complete ui and functionality

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,18 +1,24 @@
 import React, { useEffect, useState } from "react";
 import arrow from "./arrow.svg";
-import question from "./question.svg";
+import questionIcon from "./question.svg";
 import classes from "./quiz.module.css";
 
-interface QuestionS {
-  response_code: number;
-  results: {
-    category: string;
-    type: boolean;
-    difficulty: string;
-    question: string;
-    correct_answer: boolean;
-    incorrect_answers: string[];
-  }[];
+interface QuestionType {
+  question: string;
+  ans: boolean;
+}
+
+const firstQuestion: QuestionType = {
+  question: "you know about future?",
+  ans: false,
+};
+
+function setVariable(color: string): void {
+  document.documentElement.style.setProperty("--dark", `var(--${color}-dark)`);
+  document.documentElement.style.setProperty(
+    "--light",
+    `var(--${color}-light)`
+  );
 }
 
 function decode(str: string): string {
@@ -22,40 +28,94 @@ function decode(str: string): string {
   return txt.value;
 }
 
+function fetchQuiz(
+  fn: React.Dispatch<React.SetStateAction<QuestionType>>
+): void {
+  fetch("https://opentdb.com/api.php?amount=1&category=18&type=boolean")
+    .then((res) => res.json())
+    .then((data) => {
+      fn({
+        question: data.results[0].question,
+        ans: Boolean(data.results[0].correct_answer),
+      });
+    });
+}
+
 const Quiz: React.FunctionComponent = () => {
-  const [q, setQ] = useState<null | QuestionS>(null);
-  const [ani, setAni] = useState(false);
+  const [question, setQuestion] = useState<QuestionType>(firstQuestion);
+  const [animation, setAnimation] = useState(false);
+  const [ans, setAns] = useState<null | "true" | "false">(null);
 
   useEffect(() => {
-    fetch("https://opentdb.com/api.php?amount=1&category=18&type=boolean")
-      .then((res) => res.json())
-      .then(setQ);
+    fetchQuiz(setQuestion);
   }, []);
 
   const handler = () => {
-    setAni((a) => !a);
-    fetch("https://opentdb.com/api.php?amount=1&category=18&type=boolean")
-      .then((res) => res.json())
-      .then(setQ);
+    setAnimation(true);
+    setVariable("p");
+    setAns(null);
+    fetchQuiz(setQuestion);
   };
 
-  const questionText = q?.results[0].question ?? "";
+  const handleSubmit = (ans: boolean): void => {
+    if (question.ans === ans) {
+      setAns("true");
+      setVariable("green");
+      return;
+    } else if (question.ans != null) {
+      setAns("false");
+      setVariable("red");
+      return;
+    }
+    setVariable("p");
+    setAns(null);
+  };
+
+  const quizComputations = (): { class: string; text: string } => {
+    const r = {
+      class: classes.quiz,
+      text: "",
+    };
+
+    if (animation) {
+      r.class += " " + classes.animate;
+    }
+
+    if (ans != null) {
+      if (ans === "true") {
+        r.text = "correct answer ✅";
+      } else {
+        r.text = "wrong answer ❌";
+      }
+    }
+    return r;
+  };
+
+  const computed = quizComputations();
 
   return (
     <>
       <div
-        className={`${classes.quiz} ${ani ? classes.animate : ""}`}
-        onAnimationEnd={() => setAni(false)}
+        className={computed.class}
+        onAnimationEnd={() => setAnimation(false)}
       >
-        <img className={classes.svg} src={question} alt="?" />
+        <img className={classes.svg} src={questionIcon} alt="?" />
         <div className={classes.question}>
           <span>Q.</span>
-          <span>{decode(questionText)}</span>
+          <span>{decode(question.question)}</span>
         </div>
         <div className={classes.bgroup}>
-          <button className={classes.button}>True</button>
-          <button className={classes.button}>False</button>
+          <button className={classes.button} onClick={() => handleSubmit(true)}>
+            True
+          </button>
+          <button
+            className={classes.button}
+            onClick={() => handleSubmit(false)}
+          >
+            False
+          </button>
         </div>
+        <span className={classes.note}>{computed.text}</span>
       </div>
       <button className={classes.arrow} onClick={handler}>
         <img src={arrow} alt="?" />
